856ec56f57f059cf0328888666256e661c5d4625
khj1645 <khj164552@gmail.com>
2022-09-28 22:19:05 +0900

    drill 07 완성
    2021184011 김한준
    소스코드 파일 이름 변경하여 재커밋

--- DRILL_#7/anim_viwer.py
+++ DRILL_#7/anim_viwer.py
@@ -1,16 +1,19 @@
 from pico2d import *
 open_canvas()
 character = load_image('sprite.png')
+
 def draw_anim(frame,y):
     for x in range(0,frame,1):
         clear_canvas()
         character.clip_draw(55 * x, y, 55, 63, 400, 300)
         update_canvas()
         delay(0.1)
+        
 while True:
     for y in range(0,5):
         draw_anim(4,378 - 63 * y)
     for y in range(0,2):
         draw_anim(5,63 - 63 * y)
+        
 close_canvas()
 
